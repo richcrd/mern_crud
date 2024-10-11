5cd5d3bf68c27c2204e55685c13c4c8712338016
Richard <fam.castro99@gmail.com>
2024-10-11 12:18:57 -0600

    Add: Build

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 import router from "./routes/record.js";
 
 // Middleware
-const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "http://localhost:10000",
+}
+const PORT = process.env.PORT || 10000;
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
@@ -16,4 +19,6 @@ app.use("/record", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+}).catch((error) => {
+  console.log(error);
 });
